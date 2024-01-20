ad0539190cffc3787ede51e36ef55e5d4128cf5a
magnouvean <c6af1d08-d039-472f-b890-6eaf2e96e961@anonaddy.me>
2024-01-20 01:38:18 +0100

    Final fix for desktop encryption

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -13,7 +13,7 @@
   boot.initrd.luks.devices = {
     root = {
       name = "root";
-      device = "/dev/disk/by-uuid/009b54b4-d4a1-427f-8471-1ea2fcd52a1b";
+      device = "/dev/disk/by-id/wwn-0x50014ee20fa211a7";
       preLVM = true;
       allowDiscards = true;
     };
