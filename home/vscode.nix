9902976ce8a5a93537a1b2de85f529861c6d48d0
magnouvean <c6af1d08-d039-472f-b890-6eaf2e96e961@anonaddy.me>
2024-02-20 20:32:49 +0100

    Change R and python repls

--- home/vscode.nix
+++ home/vscode.nix
@@ -34,6 +34,16 @@
       "julia.symbolCacheDownload" = true;
       "notebook.formatOnSave.enabled" = true;
       "vim.highlightedyank.enable" = true;
+      "r.bracketedPaste" = true;
+      "r.rterm.linux" = "/bin/sh";
+      "r.rterm.option" = [
+        "radian"
+      ];
+      "python.terminal.launchArgs" = [
+        "-m"
+        "IPython"
+        "--no-autoindent"
+      ];
       "nix.serverSettings" = {
         "nil" = {
           "formatting" = {
